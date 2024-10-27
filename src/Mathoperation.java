6b046e858c13202ee8a7906721638eb459245a42
Abdullahasm786 <abdullahasm27@gmail.com>
2024-10-27 20:28:33 +0530

    day5first

--- src/Mathoperation.java
+++ src/Mathoperation.java
@@ -2,10 +2,14 @@ public class Mathoperation {
     public static  int multiply(int a, int b){
         return a*b;
     }
+    {
+        public static  double  multiply(double i, double j){
+        return i*j;
+    }
 }
 class Methodoverloading{
     public static void main(String args[]){
         System.out.println(Mathoperation.multiply(20,30));
-        System.out.println(Mathoperation.multiply(20, 30));
+        //System.out.println(Mathoperation.multiply(2.4,7.0));
     }
 }
