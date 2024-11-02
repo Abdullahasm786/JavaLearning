45b49156f26d0f1ff58bbfb92fdb99344084f54a
Abdullahasm786 <abdullahasm27@gmail.com>
2024-11-02 22:34:16 +0530

    day5firstmd

---
+++ src/Load1.java
@@ -0,0 +1,24 @@
+public class Mathoperation {
+    public static  int multiply(int a, int b){
+        return a*b;
+    }
+
+    {
+        public static  double  multiply(double b,double d){
+        return b*d;
+    }
+    }
+    class Load1{
+        public static void main(String args[]){
+            System.out.println(Mathoperation.multiply(20,30));
+            System.out.println(Mathoperation.multiply(2.4,7.0));
+            System.out.println(Mathoperation.multiply(20,30));
+            System.out.println(Mathoperation.multiply(2.4,7.0));
+            System.out.println(Mathoperation.multiply(20,30));
+            System.out.println(Mathoperation.multiply(2.4,7.0));
+            System.out.println(Mathoperation.multiply(20,30));
+            System.out.println(Mathoperation.multiply(2.4,7.0));
+
+        }
+    }
+}
